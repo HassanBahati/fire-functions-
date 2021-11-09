4a9ca9b7bb6dc8023c557312bf85d3018ca5f0bf
HassanBahati <mukisabahati@gmail.com>
2021-11-09 22:08:18 +0300

    edit api function

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,23 @@
 const functions = require("firebase-functions");
 
 //http function
-exports.helloworld = functions.https.onRequest((req,res)=>{
-    res.send("Hello from firebase function")
-})
+exports.helloworld = functions.https.onRequest((req, res) => {
+  res.send("Hello from firebase function");
+});
 
-
-exports.api = functions.https.onRequest((req,res)=>{
-    switch(req.method){
-        case 'GET':
-            res.send("it was a get request ")
-        break;
-        case 'POST':
-            res.send("it was a get request ")
-        break;
-        case 'DELETE':
-        res.send("it was a get request ")
-    break;
-    }
-})
+exports.api = functions.https.onRequest((req, res) => {
+  switch (req.method) {
+    case "GET":
+      res.send("it was a get request ");
+      break;
+    case "POST":
+    const body = req.body;
+      res.send(body);
+      break;
+    case "DELETE":
+      res.send("it was a delete request ");
+      break;
+    default:
+      res.send("it was a default request ");
+  }
+});
