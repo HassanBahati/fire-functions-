a24f66de30b8d5715f8bc9cd46f049a344b0fece
HassanBahati <mukisabahati@gmail.com>
2021-11-09 22:14:01 +0300

    add get request to external api

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,19 @@
 const functions = require("firebase-functions");
+const axios = require("axios");
 
 //http function
 exports.helloworld = functions.https.onRequest((req, res) => {
   res.send("Hello from firebase function");
 });
 
-exports.api = functions.https.onRequest((req, res) => {
+exports.api = functions.https.onRequest(async (req, res) => {
   switch (req.method) {
     case "GET":
-      res.send("it was a get request ");
+      const response = await axios.get("https://jsonplaceholder.com/users/1");
+      res.send(response.data);
       break;
     case "POST":
-    const body = req.body;
+      const body = req.body;
       res.send(body);
       break;
     case "DELETE":
